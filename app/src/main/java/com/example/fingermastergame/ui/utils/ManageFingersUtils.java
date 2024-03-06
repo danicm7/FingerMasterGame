ab37499624c9674c9e25696c2e20dacbb1fc7692
danicm7 <danicarrionmarin@gmail.com>
2024-03-06 18:56:32 +0100

    player history issue resume

---
+++ app/src/main/java/com/example/fingermastergame/ui/utils/ManageFingersUtils.java
@@ -0,0 +1,12 @@
+package com.example.fingermastergame.ui.utils;
+
+import android.content.Context;
+
+import com.example.fingermastergame.navigationDrawerMenu;
+
+ public class ManageFingersUtils {
+
+    public static Context getContext(){
+        return new navigationDrawerMenu().getContext();
+    }
+}
