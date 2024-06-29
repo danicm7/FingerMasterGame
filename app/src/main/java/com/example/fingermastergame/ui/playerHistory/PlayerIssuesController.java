cd40c23ab596b74ecd251181cc62acc2009b08cc
Dani <danicarrionmarin@gmail.com>
2024-06-29 11:40:33 +0200

    unify data acces

--- app/src/main/java/com/example/fingermastergame/ui/playerHistory/PlayerIssuesController.java
+++ app/src/main/java/com/example/fingermastergame/ui/playerHistory/PlayerIssuesController.java
@@ -8,16 +8,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import com.example.fingermastergame.R;
+import com.example.fingermastergame.storage.ManageStorage;
 import com.example.fingermastergame.ui.playerData.PlayerModel;
 import com.example.fingermastergame.ui.playerData.PlayersListModel;
-import com.example.fingermastergame.ui.utils.ManageFingersUtils;
-import com.google.gson.Gson;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class PlayerIssuesController extends AppCompatActivity {
@@ -58,34 +51,8 @@ public class PlayerIssuesController extends AppCompatActivity {
         }
     }
     private ArrayList<PlayerModel> loadPlayers() {
-        ArrayList<PlayerModel> playersList = new ArrayList<PlayerModel>();
-        final File file = new File(getFilesDir(), getResources().getString(R.string.player_data_file_name));
-        final Gson gson = new Gson();
-        if (!file.exists()){
-            return playersList;
-        }
-        try {
-            final FileInputStream fis = openFileInput(file.getName());
-            final InputStreamReader inputStreamReader = new InputStreamReader(fis, StandardCharsets.UTF_8);
-            final StringBuilder stringBuilder = new StringBuilder();
-            final BufferedReader reader = new BufferedReader(inputStreamReader);
-            final ManageFingersUtils utils =  ManageFingersUtils.getInstance();
-            String line = reader.readLine();
-            while (line != null) {
-                stringBuilder.append(line).append('\n');
-                line = reader.readLine();
-            }
-            final String contents = stringBuilder.toString();
-            final PlayerModel[] data = gson.fromJson(contents, PlayerModel[].class);
-
-            fis.close();
-            inputStreamReader.close();
-            reader.close();
-            playersList = utils.arrayToArrayList(data);
-            return playersList;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return playersList;
-        }
+        final ArrayList<PlayerModel> list;
+        list = ManageStorage.loadPlayers(getApplicationContext());
+        return list;
     }
 }
